b59b5cb5a66c7c513400d4b5ad2d9f1a5b7d28af
rdivakaran <riyadivakaran03@gmail.com>
2025-03-12 23:54:57 -0400

    leakCanary and Firebase

--- app/src/main/java/com/vipulasri/jetinstagram/MainActivity.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.vipulasri.jetinstagram
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import com.google.firebase.messaging.FirebaseMessaging
 import com.vipulasri.jetinstagram.ui.MainScreen
 import com.vipulasri.jetinstagram.ui.theme.JetInstagramTheme
 
 class MainActivity : AppCompatActivity() {
+  private val TAG: String = "MainActivityLogs"
 
   @ExperimentalFoundationApi
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +24,27 @@ class MainActivity : AppCompatActivity() {
         Surface(color = MaterialTheme.colors.background) {
           MainScreen()
         }
+
       }
     }
+    //get registration token
+    FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+      if (!task.isSuccessful) {
+        Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+      }else {
+
+        // Get new FCM registration token
+        val token = task.result
+
+        // Log and toast
+//        val msg = getString(R.string.msg_token_fmt, token)
+        Log.d(TAG, "Token Received = $token")
+        Toast.makeText(baseContext, "Got the token!", Toast.LENGTH_SHORT).show()
+      }
+
+    }
+
+
   }
 
 }
---
+++ app/src/main/java/com/vipulasri/jetinstagram/MyFirebaseService.kt
@@ -0,0 +1,73 @@
+package com.vipulasri.jetinstagram
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseService : FirebaseMessagingService() {
+
+    val TAG = "FirebaseNotification: "
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.d(TAG, "From: ${message.from}")
+
+        // Check if message contains a data payload.
+        if (message.notification != null) {
+            Log.d(TAG, "Message data payload: ${message.data}")
+            handleNow(message)
+
+
+        }
+
+    }
+
+    fun handleNow(messageBody: RemoteMessage) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val requestCode = 0
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            requestCode,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE,
+        )
+
+        val channelId = "fcm_default_channel"
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(messageBody?.notification?.title)
+            .setContentText(messageBody?.notification?.body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT,
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notificationId = 0
+        notificationManager.notify(notificationId, notificationBuilder.build())
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+}
